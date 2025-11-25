f0c7d189f8c73592b1323f0504fa2d09b81867f4
Kuria300 <eugenekuria66@gmail.com>
2025-11-25 19:28:17 +0300

    closures

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+function createLoginTracker(userInfo){
+  let attemptCount=0
+  let maxAttempts=3
 
+  const {password}= userInfo
 
+    return(passwordAttempt)=>{
 
+      //check first if its been locked
+      if(attemptCount > maxAttempts){
+         return 'Account locked due to too many failed login attempts'
+      }
+
+      attemptCount++
+      if(passwordAttempt === password){
+        attemptCount=0
+        return 'Login successful'
+      }else{
+  
+         if(attemptCount > maxAttempts){
+           return 'Account locked due to too many failed login attempts'
+         }else{
+           return `Attempt ${attemptCount}: Login failed`;
+         }
+      }
+
+    }
+
+}
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
